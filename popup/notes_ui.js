a0e047d59ff7da7c2a05f08824f785a7a2008a91
swpease <swpease@gmail.com>
2017-01-11 11:47:00 -0800

    added getVideoTitleAndId

--- popup/notes_ui.js
+++ popup/notes_ui.js
@@ -1,11 +1,26 @@
 function setData(message) {
   /* Handles the video data sent from the video_data_getter content script.
-    Message is a JSON object with pauseTime, videoUrl, and videoTitle.
+    Message is a JSON object with pauseTime.
   */
   var pauseTime = message.pauseTimeKey;
   var pauseElement = document.getElementById("pauseTime");
   pauseElement.textContent = pauseTime;
 
+}
+
+function getTitleAndVideoId() {
+  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+  gettingActiveTab.then((tabs) => {
+    var tab = tabs[0];
+
+    var browserTitle = tab.title;
+    var videoTitle = browserTitle.substring(0, browserTitle.length - 10);  // removes " - YouTube"
+
+    var url = tab.url;
+    var suffix = url.split("?v=")[1];
+    var videoId = suffix.split("&")[0];
+    // return the vals
+  });
 }
 
 browser.runtime.onMessage.addListener(setData);
@@ -15,14 +30,10 @@ document.addEventListener("click", (e) => {
     var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
     gettingActiveTab.then((tabs) => {
       browser.tabs.sendMessage(tabs[0].id, {tab: tabs[0]});
+      window.close();
     });
-    // window.close();
   }
 });
 
 browser.tabs.executeScript(null, { file: "/content_scripts/video_data_getter.js" });
 browser.tabs.executeScript(null, { file: "/content_scripts/video_unpauser.js" });
-
-// var myTitle = message.tab.title;
-// var shortTitle = myTitle.substring(0, myTitle.length - 10);
-// console.log(shortTitle);
