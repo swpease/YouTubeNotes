a703a9500ef0b7bdf3068283dde902f216900c1c
swpease <swpease@gmail.com>
2017-01-27 14:06:16 -0800

    The New note event functionality complete
    Finished UI component for making new notes.

--- content_scripts/yt_ui.js
+++ content_scripts/yt_ui.js
@@ -7,7 +7,7 @@ function makeHTML(input){
   return dummy.firstChild;
 }
 
-var notesWrapper_raw = '<div id="notes-wrapper" class="yt-card yt-card-has-padding"></div>';
+var notesSection_raw = '<div id="notes-section" class="yt-card yt-card-has-padding"></div>';
 var noteInputWrapper_raw = '<div id="notes-input-wrapper" class="comment-simplebox-renderer"></div>';
 var noteInputDefault_raw = '<div class="comment-simplebox-renderer-collapsed comment-simplebox-trigger" tabindex="0" role="form" aria-haspopup="true"><div class="comment-simplebox-renderer-collapsed-content">Add a private note...</div><div class="comment-simplebox-arrow"><div class="arrow-inner"></div><div class="arrow-outer"></div></div></div>';
 var noteInput_raw = '<div id="note-simplebox-create-note" class="comment-simplebox-content"><div class="comment-simplebox" id="note-simplebox"><div class="comment-simplebox-arrow"><div class="arrow-inner"></div><div class="arrow-outer"></div></div>' +
@@ -16,7 +16,7 @@ var noteInput_raw = '<div id="note-simplebox-create-note" class="comment-simpleb
                     '<button class="cancel-note-button yt-uix-button yt-uix-button-size-default yt-uix-button-default comment-simplebox-cancel" type="button" onclick=";return false;"><span class="yt-uix-button-content">Cancel</span>' +
                     '</button><button class="make-note-button yt-uix-button yt-uix-button-size-default yt-uix-button-primary yt-uix-button-empty" type="button" onclick=";return false;" aria-label="Make Note" disabled="">Make Note</button></div></div></div></div>';
                     // Need to add focused to id="note-simplebox" when clicked
-var notesWrapper = makeHTML(notesWrapper_raw);
+var notesSection = makeHTML(notesSection_raw);
 var noteInputWrapper = makeHTML(noteInputWrapper_raw);
 var noteInputDefault = makeHTML(noteInputDefault_raw);
 var noteInput = makeHTML(noteInput_raw);
@@ -27,9 +27,10 @@ var cancelNoteBtn = noteInput.getElementsByClassName("cancel-note-button")[0];
 var makeNoteBtn = noteInput.getElementsByClassName("make-note-button")[0];
 
 
-notesWrapper.appendChild(noteInputWrapper);
+notesSection.appendChild(noteInputWrapper);
 noteInputWrapper.appendChild(noteInputDefault);
 
+// BEGIN Event functionality for making a new note
 function blurNote() {
   notesBox.classList.remove("focus");
 }
@@ -38,8 +39,12 @@ function focusNote() {
   notesBox.classList.add("focus");
 }
 
+function toggleNoteButtonEnabled() {
+  note.textContent == "" ? makeNoteBtn.disabled = true : makeNoteBtn.disabled = false;
+}
+
 function switchToNoteInput() {
-  notesBox.classList.add("focus"); //remove
+  notesBox.classList.add("focus");
   noteInputWrapper.replaceChild(noteInput, noteInputDefault);
   note.focus();
 }
@@ -53,7 +58,9 @@ noteInputDefault.addEventListener('click', switchToNoteInput);
 cancelNoteBtn.addEventListener('click', switchToNoteInputDefault);
 note.addEventListener('focus', focusNote);
 note.addEventListener('blur', blurNote);
+note.addEventListener('keyup', toggleNoteButtonEnabled);
+// END Event functionalityFor making a new note
 
 var commentsSection = document.getElementById('watch-discussion');
 
-commentsSection.parentElement.insertBefore(notesWrapper, commentsSection);
+commentsSection.parentElement.insertBefore(notesSection, commentsSection);
