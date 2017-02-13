a863d717dd9d78d9034cb70fa2a9016e9b1fea42
swpease <swpease@gmail.com>
2017-02-13 10:06:58 -0800

    cleaned up
    clean up notes and files and such

--- content_scripts/yt_ui.js
+++ content_scripts/yt_ui.js
@@ -1,6 +1,4 @@
-document.body.style.border = "5px solid green";
-
-//Global.Signed bank backgroundClick.
+//Global. Assigned by background script.
 var videoId;
 var videoTitle;
 
@@ -10,39 +8,9 @@ function makeHTML(input){
   return dummy.firstChild;
 }
 
-//Converts a time in seconds to a time in format (hh):(mm):(ss) or so.
-function prettifyTime(time) {
-  var vidDuration = document.querySelector('.html5-main-video').duration;
-  var vidHrs = Math.floor(vidDuration / 3600);
-  var prettyTime = "";
-
-  time = Math.floor(time);
-  var hours = Math.floor(time / 3600);
-  var rem = time % 3600;
-  var mins = Math.floor(rem / 60);
-  var secs = rem % 60;
-
-  if (vidHrs > 0) {
-    prettyTime += hours + ":";
-    if (mins < 10) {
-      prettyTime += "0" + mins + ":";
-    } else {
-      prettyTime += mins + ":";
-    }
-  } else {
-    prettyTime += mins + ":";
-  }
-  if (secs < 10) {
-    prettyTime += "0" + secs;
-  } else {
-    prettyTime += secs;
-  }
-
-  return prettyTime;
-}
-
 
 var notesSection_raw = '<div id="notes-wrapper" class="yt-card yt-card-has-padding"></div>';
+var notesSectionHeader_raw = '<h2 class="comment-section-header-renderer" tabindex="0"><b>Notes</b></h2>';
 var noteInputWrapper_raw = '<div id="notes-input-wrapper" class="comment-simplebox-renderer"></div>';
 var noteInputDefault_raw = '<div class="comment-simplebox-renderer-collapsed comment-simplebox-trigger" tabindex="0" role="form" aria-haspopup="true"><div class="comment-simplebox-renderer-collapsed-content">Add a private note...</div><div class="comment-simplebox-arrow"><div class="arrow-inner"></div><div class="arrow-outer"></div></div></div>';
 var noteInput_raw = '<div id="note-simplebox-create-note" class="comment-simplebox-content"><div class="comment-simplebox" id="note-simplebox"><div class="comment-simplebox-arrow"><div class="arrow-inner"></div><div class="arrow-outer"></div></div>' +
@@ -50,8 +18,9 @@ var noteInput_raw = '<div id="note-simplebox-create-note" class="comment-simpleb
                     '<div class="comment-simplebox-controls"><div class="comment-simplebox-buttons">' +
                     '<button class="cancel-note-button yt-uix-button yt-uix-button-size-default yt-uix-button-default comment-simplebox-cancel" type="button" onclick=";return false;"><span class="yt-uix-button-content">Cancel</span>' +
                     '</button><button class="confirm-note-button yt-uix-button yt-uix-button-size-default yt-uix-button-primary yt-uix-button-empty" type="button" onclick=";return false;" aria-label="Make Note" disabled="">Make Note</button></div></div></div></div>';
-                    // Need to add focused to id="note-simplebox" when clicked
+
 var notesSection = makeHTML(notesSection_raw);
+var notesSectionHeader = makeHTML(notesSectionHeader_raw);
 var noteInputWrapper = makeHTML(noteInputWrapper_raw);
 var noteInputDefault = makeHTML(noteInputDefault_raw);
 var noteInput = makeHTML(noteInput_raw);
@@ -64,6 +33,7 @@ var makeNoteBtn = noteInput.getElementsByClassName("confirm-note-button")[0];
 
 notesSection.appendChild(noteInputWrapper);
 noteInputWrapper.appendChild(noteInputDefault);
+notesSection.insertBefore(notesSectionHeader, noteInputWrapper);
 
 // BEGIN Event functionality for making a new note
 function blurNote(commentSimplebox) {
@@ -138,15 +108,11 @@ note.addEventListener('keyup', function () { toggleNoteButtonEnabled(note, makeN
 var savedNotesWrapper_raw = '<div class="comment-section-renderer-items" id="saved-notes-section-renderer-items"></div>';
 // wrapper for noteRenderer (To switch with the edit textbox)
 var noteRendererWrapper_raw = '<section class="note-renderer comment-thread-renderer"></section>'
-// contains a single note. Also includes footer container (two outer layers). Also includes button for Edit/Delete popup.
+// contains a single note. Also includes footer.
 var noteRenderer_raw = '<div class="comment-renderer"><div class="comment-renderer-content"><div class="note-renderer-footer comment-renderer-footer"><div class="comment-action-buttons-toolbar"><button type="button" class="note-footer-edit-button note-footer-btn notes-footer-item yt-uix-button yt-uix-button-link">Edit</button><button type="button" class="notes-footer-item note-footer-spacer yt-uix-button yt-uix-button-link">•</button><button type="button" class="note-footer-delete-button note-footer-btn notes-footer-item yt-uix-button yt-uix-button-link">Delete</button></div></div></div></div>';
-// header. To add: a fn to set the currentTime = [text] and the text for the <span>. Added my own class 'note-video-time' for future use... Modded <a> to <span>
 var noteHeader_raw = '<div class="comment-renderer-header"><span class="comment-author-text note-video-time" style="cursor:pointer;">Time goes here</span></div>';
-// text content. To add: the text content. Added my own class 'note-text-content' for future use...
 var noteContent_raw = '<div class="comment-renderer-text" tabindex="0" role="article"><div class="comment-renderer-text-content note-text-content">Note goes here</div></div>';
-
-// For the editing notes.Note to that YouTubeAs a single element that day Switcheroo and as needed.Not sure if faster.
-//Me
+// Just like the noteInput section.
 var noteEdit_raw = '<div class="comment-renderer comment-renderer-editing"><div class="comment-simplebox-edit comment-simplebox-content">' +
                    '<div class="comment-simplebox focus"><div class="comment-simplebox-arrow"><div class="arrow-inner"></div><div class="arrow-outer"></div></div>' +
                    '<div class="comment-simplebox-frame"><div class="comment-simplebox-prompt"></div><div class="note-simplebox-text comment-simplebox-text" role="textbox" aria-multiline="true" contenteditable="true"></div></div>' +
@@ -158,6 +124,36 @@ var noteEdit_raw = '<div class="comment-renderer comment-renderer-editing"><div 
 var savedNotesWrapper = makeHTML(savedNotesWrapper_raw);
 notesSection.appendChild(savedNotesWrapper);
 
+//Converts a time in seconds to a time in format (hh):(mm):(ss) (ish).
+function prettifyTime(time) {
+  var vidDuration = document.querySelector('.html5-main-video').duration;
+  var vidHrs = Math.floor(vidDuration / 3600);
+  var prettyTime = "";
+
+  time = Math.floor(time);
+  var hours = Math.floor(time / 3600);
+  var rem = time % 3600;
+  var mins = Math.floor(rem / 60);
+  var secs = rem % 60;
+
+  if (vidHrs > 0) {
+    prettyTime += hours + ":";
+    if (mins < 10) {
+      prettyTime += "0" + mins + ":";
+    } else {
+      prettyTime += mins + ":";
+    }
+  } else {
+    prettyTime += mins + ":";
+  }
+  if (secs < 10) {
+    prettyTime += "0" + secs;
+  } else {
+    prettyTime += secs;
+  }
+
+  return prettyTime;
+}
 
 // Call find display nodeSo that you know it's areDisplayIn order Vaitai
 function insertByTime(note) {
@@ -213,7 +209,7 @@ function displayNote(noteTime, noteText) {
   noteFooter.parentElement.insertBefore(noteHeader, noteFooter);
   noteFooter.parentElement.insertBefore(noteContent, noteFooter);
 
-
+  // Edit note.
   footerEditBtn.addEventListener('click', function () {
     var noteKeyTime = noteRendererWrapper.dataset.noteTime;  // For lookup by makeEditBtn click
 
@@ -225,13 +221,15 @@ function displayNote(noteTime, noteText) {
     var cancelEditBtn = noteEdit.getElementsByClassName("cancel-note-button")[0];
     var makeEditBtn = noteEdit.getElementsByClassName("confirm-note-button")[0];
 
+    edit.addEventListener('focus', function () { focusNote(editBox) });
+    edit.addEventListener('blur', function () { blurNote(editBox) });
+    edit.addEventListener('keyup', function () { toggleNoteButtonEnabled(edit, makeEditBtn, noteText) });
+
     cancelEditBtn.addEventListener('click', function () {
       noteEdit.parentElement.replaceChild(noteRenderer, noteEdit);
       noteEdit.remove();
     });
-    edit.addEventListener('focus', function () { focusNote(editBox) });
-    edit.addEventListener('blur', function () { blurNote(editBox) });
-    edit.addEventListener('keyup', function () { toggleNoteButtonEnabled(edit, makeEditBtn, noteText) });
+
     makeEditBtn.addEventListener('click', function () {
       var editedText = noteEdit.querySelector('.note-simplebox-text').textContent;
 
@@ -251,11 +249,13 @@ function displayNote(noteTime, noteText) {
         });
       });
     })
+
+    // Display
     noteRenderer.parentElement.replaceChild(noteEdit, noteRenderer);
     placeCaretAtEnd(edit);
   });
 
-  //DeleteNote
+  // Delete Note
   footerDelBtn.addEventListener('click', function () {
     var gettingItem = browser.storage.local.get(videoId);
     gettingItem.then((result) => {
@@ -277,12 +277,14 @@ function displayNote(noteTime, noteText) {
       }
     });
   });
+
+  // Seek to note's time.
   noteTimeElement.addEventListener('click', function() { document.querySelector('.html5-main-video').currentTime = noteTime; });
 
   insertByTime(noteRendererWrapper);
 }
 
-//GettingURLHey infoVideo ID andTitle.
+// Getting background info: Video ID and Title.
 function initialize(message) {
   videoId = message.id;
   videoTitle = message.title;
@@ -306,13 +308,9 @@ function initialize(message) {
   });
 }
 
-function handleError(error) {
-  console.log(`Error: ${error}`);
-}
-
 browser.runtime.onMessage.addListener(initialize);
 browser.runtime.sendMessage({});
-//And back transcript interaction
+// End background script interaction
 
-var commentsSection = document.getElementById('watch-discussion');
-commentsSection.parentElement.insertBefore(notesSection, commentsSection);
+var detailsSection = document.getElementById('action-panel-details');
+detailsSection.parentElement.insertBefore(notesSection, detailsSection.nextSibling);
