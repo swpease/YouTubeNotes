21a2ee5c4e47ced5d65fa0491c4835b72c3d5fdc
swpease <swpease@gmail.com>
2017-02-26 14:17:35 -0800

    note population and links working
    Notes successfully populated in the pop-up and link to the video at the
    correct time upon clicking.

--- browser_action/popup.js
+++ browser_action/popup.js
@@ -1,4 +1,49 @@
-var addBtn = document.querySelector('.add');
-addBtn.addEventListener('click', function() {
-  browser.tabs.update({url: 'https://developer.mozilla.org'});
-})
+  function setupExistingNotes() {
+  var gettingSavedNotes = browser.storage.local.get();
+  gettingSavedNotes.then((result) => {
+    if (Array.isArray(result)) {  // If Firefox version less than 52.
+      result = result[0];
+    }
+
+    var videoIds = Object.keys(result);
+    var videoNotesContainer = document.querySelector('.video-notes-container');
+
+    if (videoIds.length == 0) {
+      return;
+    }
+
+    for (var videoId of videoIds) {
+      var baseUrl = "https://www.youtube.com/watch?v=" + videoId;
+
+      var videoNotes = document.createElement('div');
+      videoNotes.setAttribute('class', 'video-notes');
+      videoNotesContainer.appendChild(videoNotes);
+
+      var videoTitle = result[videoId]["title"];
+      var titleEl = document.createElement('h2');
+      titleEl.textContent = videoTitle;
+      titleEl.addEventListener('click', function() {
+          browser.tabs.update({url: baseUrl});
+          window.close();
+      })
+      videoNotes.appendChild(titleEl);
+
+      var savedNotes = result[videoId]["notes"];
+      for (let noteTime of Object.keys(savedNotes)) {
+        const noteUrl = baseUrl + "&t=" + Math.floor(noteTime) + "s";
+        var noteText = savedNotes[noteTime];
+
+        var note = document.createElement('div');
+        note.setAttribute('class', 'note');
+        note.innerHTML = noteText;
+        note.addEventListener('click', function() {
+          browser.tabs.update({url: noteUrl});
+          window.close();
+        })
+        videoNotes.appendChild(note);
+      }
+    }
+  });
+}
+
+setupExistingNotes();
